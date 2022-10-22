63ffbd8b992f4a9c5265b044b00615e8759d88b6
tamco61 <42232243+tamco61@users.noreply.github.com>
2022-10-22 19:47:14 +0300

    22.10.2022 ALgos + front

---
+++ algorithms.py
@@ -0,0 +1,86 @@
+from data.driver import Driver
+from data.road import Road
+from data.point import Point
+from data import db_session
+
+import math
+
+
+BUS_SPEED = 500 # met/min
+
+
+class Event:
+    def __init__(self, type, time, number, gate, count):
+        self.type = type
+        self.time = time
+        self.number = number
+        self.gate = gate
+        self.count = count
+
+    def get_type(self):
+        return self.type
+
+
+def check_free_bus():
+    session = db_session.create_session()
+    free_drivers = session.query(Driver).filter(Driver.status == 0).all()
+    free_drivers_on_50 = []
+    free_drivers_on_100 = []
+
+    return free_drivers_on_50, free_drivers_on_100
+
+
+def find_bus_count(d50, d100, event):
+    count = event.count
+    count_100 = len(d100)
+    count_50 = len(d50)
+    n100 = 0
+    n50 = 0
+
+    if count_100 <= count // 100:
+        n100 += count_100
+        count_100 = 0
+    else:
+        n100 += count // 100
+        count_100 -= n100
+    count -= n100 * 100
+
+    if count_50 <= count // 50:
+        n50 += count_50
+    else:
+        if count_100 == 0:
+            n50 = math.ceil(count / 50)
+        elif 50 > count:
+            n50 = 1
+        elif count_50 >= 2:
+            if abs(count - 50) > abs(count - 100):
+                n50 = 2
+            else:
+                n100 += 1
+    return n50, n100
+
+
+def func(x, session, point_id):
+    x = session.query(Road).filter(Road.source_point_id == x.point and Road.target_point_id == point_id).first().distance
+    if not x:
+        return 3
+    else:
+        return x / 500
+
+
+def sort_for_low_distance(d50, d100, point_id):
+    session = db_session.create_session()
+    d50.sort(d50,
+             key=lambda x:func(x, session, point_id))
+
+    d100.sort(d100,
+             key=lambda x: func(x, session, point_id))
+
+    return d50, d100
+
+
+def choice_bus(d50, d100, n50, n100):
+    return d50[:n50], d100[:n100]
+
+
+
---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, redirect, request, abort, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import PasswordField, TextAreaField, StringField, SubmitField, BooleanField, IntegerField
+from flask_login import current_user, LoginManager, login_user, logout_user, login_required
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+db_session.global_init("db/database.sqlite")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        if current_user.privileges == 0:
+            return render_template("driver.html", title='Водитель')
+        elif current_user.privileges == 1:
+            return render_template("dispatcher.html", title='Диспетчер')
+        elif current_user.privileges == 2:
+            return render_template("leader.html", title='Руководитель')
+    else:
+        return render_template("login.html", title='Авторизация')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.username == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
